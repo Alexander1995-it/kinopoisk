972bb6cea57b90de1fcec0b8ec47806f5ab39270
Alexander1995-it <sany_nax@mail.ru>
2023-07-03 15:55:13 +0300

    fix feature_1

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import Movies from "components/Movies/Movies";
 import InfoMovie from "components/InfoMovie/InfoMovie";
 
 function App() {
-    console.log('feature')
+    console.log('feature_1')
     return (
         <div className={style.appBlock}>
             <Routes>
